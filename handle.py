ad6204107e5bf190acf195cb8918a631c0767e91
quininer <quininer@live.com>
2015-04-15 19:11:35 +0800

    generate RPL events

--- handle.py
+++ handle.py
@@ -40,54 +40,21 @@ def NOTICE(bot, kwargs):
 def QUIT(bot, kwargs):
     yield from wait([m.quit(kwargs) for m in bot.modules])
 
-@e.on('RPL_WELCOME')
-def RPL_WELCOME(bot, kwargs):
-    yield from wait([m.rpl_welcome(kwargs) for m in bot.modules])
-
-@e.on('RPL_YOURHOST')
-def RPL_YOURHOST(bot, kwargs):
-    yield from wait([m.rpl_yourhost(kwargs) for m in bot.modules])
-
-@e.on('RPL_CREATED')
-def RPL_CREATED(bot, kwargs):
-    yield from wait([m.rpl_created(kwargs) for m in bot.modules])
-
-@e.on('RPL_MYINFO')
-def RPL_MYINFO(bot, kwargs):
-    yield from wait([m.rpl_myinfo(kwargs) for m in bot.modules])
-
-@e.on('RPL_BOUNCE')
-def RPL_BOUNCE(bot, kwargs):
-    yield from wait([m.rpl_bounce(kwargs) for m in bot.modules])
-
-@e.on('RPL_MOTDSTART')
-def RPL_MOTDSTART(bot, kwargs):
-    yield from wait([m.rpl_motdstart(kwargs) for m in bot.modules])
-
-@e.on('RPL_MOTD')
-def RPL_MOTD(bot, kwargs):
-    yield from wait([m.rpl_motd(kwargs) for m in bot.modules])
-
-@e.on('RPL_ENDOFMOTD')
-def RPL_ENDOFMOTD(bot, kwargs):
-    yield from wait([m.rpl_endofmotd(kwargs) for m in bot.modules])
-
-@e.on('RPL_LUSERCLIENT')
-def RPL_LUSERCLIENT(bot, kwargs):
-    yield from wait([m.rpl_luserclitent(kwargs) for m in bot.modules])
-
-@e.on('RPL_LUSERME')
-def RPL_LUSERME(bot, kwargs):
-    yield from wait([m.rpl_luserme(kwargs) for m in bot.modules])
-
-@e.on('RPL_LUSEROP')
-def RPL_LUSEROP(bot, kwargs):
-    yield from wait([m.rpl_luserop(kwargs) for m in bot.modules])
-
-@e.on('RPL_LUSERUNKNOWN')
-def RPL_LUSERUNKNOWN(bot, kwargs):
-    yield from wait([m.rpl_luserunknown(kwargs) for m in bot.modules])
-
-@e.on('RPL_LUSERCHANNELS')
-def RPL_LUSERCHANNELS(bot, kwargs):
-    yield from wait([m.rpl_luserchannels(kwargs) for m in bot.modules])
+for event in [
+    'RPL_WELCOME',
+    'RPL_YOURHOST',
+    'RPL_CREATED',
+    'RPL_MYINFO',
+    'RPL_BOUNCE',
+    'RPL_MOTDSTART',
+    'RPL_MOTD',
+    'RPL_ENDOFMOTD',
+    'RPL_LUSERCLIENT',
+    'RPL_LUSERME',
+    'RPL_LUSEROP',
+    'RPL_LUSERUNKNOWN',
+    'RPL_LUSERCHANNELS',
+]:
+    e.on(event)(
+        (lambda bot, kwargs: (yield from wait([getattr(m, event.lower())(kwargs) for m in bot.modules])))
+    )
